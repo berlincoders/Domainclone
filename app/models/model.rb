ac59c0bb60d17e043810c0a10d90cde57ca92775
Romeo Asinari <ralonsoad@gmail.com>
2023-02-19 01:51:53 +0100

    Before adding the navbar

--- app/models/model.rb
+++ app/models/model.rb
@@ -2,5 +2,7 @@ class Model < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,Trackable,Confirmable 
+
+  has_many :properties       
 end
